223d0c3bbe6e1b395c6d07ff0431cb468b23ed8d
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-03-26 15:40:30 -0400

    create connection.

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,15 @@ config()
 
 createConnection({
 	type: "postgres",
-	
-
+	host: process.env.POSTGRES_HOST,
+	username: process.env.POSTGRES_USERNAME,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
+	synchronize: true,
+	logging: true,
+	entities: [__dirname + "/database/entities/**/**.{ts,js}"],
+	migrations: [__dirname + "/database/migrations/**/*.{ts,js}"],
+	subscribers: [__dirname + "/database/subscribers/**/*.{ts,js}"]
 }).then(() => {
-	// parse port
-})
+	console.log("Connected to the database.");
+});
