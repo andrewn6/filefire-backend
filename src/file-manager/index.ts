1e89b17af319adac576ca8a85cefdb213c4b87d9
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-03-25 21:29:28 -0400

    file manager for panel, delete/upload

---
+++ src/file-manager/index.ts
@@ -0,0 +1,20 @@
+import { S3 } from 'aws-sdk';
+import { get } from 'config';
+
+const s3 = new S3(get('aws'));
+
+export const uploadFile = () => {
+
+}
+
+export const deleteFile = () => {
+
+}
+
+export const renameFile = () => {
+  
+
+export const getFiles = () => {
+
+}
+
