9eeb7d834dcba16e14126c3595ffc3f65605202f
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-03-26 15:28:02 -0400

    (wip)

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+import { config } from "dotenv";
+import { createConnection } from "typeorm";
+require('express-async-errors');
+config()
+
+createConnection({
+	type: "postgres",
+
+}).then(() => {
+	// parse port
+})
