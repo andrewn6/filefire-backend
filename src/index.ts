063446c207f0c82d669215558b213affcf832ac7
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-03-26 16:50:28 -0400

    panic commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
 import { config } from "dotenv";
 import { createConnection } from "typeorm";
-require('express-async-errors');
-config()
+require("express-async-errors");
+config();
 
 createConnection({
 	type: "postgres",
